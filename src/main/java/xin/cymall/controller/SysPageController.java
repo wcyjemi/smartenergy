3bc8148adcb16507f1e5f07a00704c1b16c496fc
3304737387@qq.com <3304737387@qq.com>
2019-02-16 15:13:33 +0800

    删除无用代码

--- src/main/java/xin/cymall/controller/SysPageController.java
+++ src/main/java/xin/cymall/controller/SysPageController.java
@@ -5,10 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import xin.cymall.entity.AppBaseInfo;
-import xin.cymall.entity.AppVersion;
-import xin.cymall.service.AppBaseInfoService;
-import xin.cymall.service.AppVersionService;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,10 +19,6 @@ import java.util.List;
  */
 @Controller
 public class SysPageController {
-	@Autowired
-	private AppBaseInfoService appBaseInfoService;
-	@Autowired
-	private AppVersionService appVersionService;
 	@RequestMapping("{module}/{url}.html")
 	public String page(@PathVariable("module") String module, @PathVariable("url") String url){
 		return module + "/" + url + ".html";
@@ -35,27 +27,27 @@ public class SysPageController {
 
 	@RequestMapping("index/main")
 	public String main(Model model){
-		List<AppBaseInfo> appBaseInfoList = appBaseInfoService.queryList(new HashMap<String, Object>());
-		List<AppVersion> appVersionList = new ArrayList<>();
-		for (int i=0;i<appBaseInfoList.size();i++){
-			AppVersion temp = new AppVersion();
-			temp.setAppId(Long.parseLong(appBaseInfoList.get(i).getId()+""));
-			temp.setAppType("1");
-			AppVersion appVersion = appVersionService.queryLastAppVer(temp);
-			AppVersion temp2 = new AppVersion();
-			temp2.setAppId(Long.parseLong(appBaseInfoList.get(i).getId()+""));
-			temp2.setAppType("2");
-			AppVersion appVersion2 = appVersionService.queryLastAppVer(temp2);
-			if (appVersion!=null){
-				appVersionList.add(appVersion);
-			}
-			if (appVersion2!=null){
-				appVersionList.add(appVersion2);
-			}
-		}
-		model.addAttribute("appList",appVersionList);
-		return "main/index";
-//		return "menu/list";
+//		List<AppBaseInfo> appBaseInfoList = appBaseInfoService.queryList(new HashMap<String, Object>());
+//		List<AppVersion> appVersionList = new ArrayList<>();
+//		for (int i=0;i<appBaseInfoList.size();i++){
+//			AppVersion temp = new AppVersion();
+//			temp.setAppId(Long.parseLong(appBaseInfoList.get(i).getId()+""));
+//			temp.setAppType("1");
+//			AppVersion appVersion = appVersionService.queryLastAppVer(temp);
+//			AppVersion temp2 = new AppVersion();
+//			temp2.setAppId(Long.parseLong(appBaseInfoList.get(i).getId()+""));
+//			temp2.setAppType("2");
+//			AppVersion appVersion2 = appVersionService.queryLastAppVer(temp2);
+//			if (appVersion!=null){
+//				appVersionList.add(appVersion);
+//			}
+//			if (appVersion2!=null){
+//				appVersionList.add(appVersion2);
+//			}
+//		}
+//		model.addAttribute("appList",appVersionList);
+//		return "main/index";
+		return "menu/list";
 	}
 
 
